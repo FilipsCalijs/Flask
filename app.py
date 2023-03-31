d7431b606f082f0dffae8320b955096e04d2ae25
FilipsCalijs <filips.calijs@gmail.com>
2023-03-31 17:27:04 +0300

    Flask конец 2-ого урока

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ def index():
 @app.route('/about')
 
 def about():
-    return render_template("index.html")
+    return render_template("about.html")
 
 
 
